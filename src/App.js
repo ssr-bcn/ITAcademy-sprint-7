115a64a83562111f38d0259e8bd7cf3b1fda11e7
SSR <ssr@estamosenello.net>
2022-05-05 22:17:19 +0200

    Ejercicio 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [web, setWeb] = useState(0);
+  const [seo, setSeo] = useState(0);
+  const [ads, setAds] = useState(0);
+  const [budget, setBudget] = useState(0);
+
+  useEffect( () => {
+    setBudget( web +seo +ads );
+  }, [web, seo, ads]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section>
+        <h2>Què vols fer?</h2>
+        <ul>
+          <li>
+            <input type="checkbox" id="web" value="500" onChange={e => setWeb( web === 0 ? +e.target.value : 0 )} />
+            <label htmlFor="web">Una pàgina web (500€)</label>
+          </li>
+          <li>
+            <input type="checkbox" id="seo" value="300" onChange={e => setSeo( seo === 0 ? +e.target.value : 0 )} />
+            <label htmlFor="seo">Una consultoria SEO (300€)</label>
+          </li>
+          <li>
+            <input type="checkbox" id="ads" value="200" onChange={e => setAds( ads === 0 ? +e.target.value : 0 )} />
+            <label htmlFor="ads">Una campanya de Google Ads (200€)</label>
+          </li>
+        </ul>
+        <p>Preu: {budget}€</p>
+      </section>
+    </>
   );
 }
 
